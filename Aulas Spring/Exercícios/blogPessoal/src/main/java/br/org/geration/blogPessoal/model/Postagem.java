103f61a997e65fbec09e31038c4e943707f40195
Guilherme <guilherme.silva307@fatec.sp.gov.br>
2021-05-29 22:53:20 -0300

    blogPessoal

--- Aulas Spring/Exercícios/blogPessoal/src/main/java/br/org/geration/blogPessoal/model/Postagem.java
+++ Aulas Spring/Exercícios/blogPessoal/src/main/java/br/org/geration/blogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity // Essa classe será uma entidade do jpa.hibernate
 @Table(name = "tb_postagem") // Essa classe virará uma tabela
@@ -30,6 +33,11 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP) //Vai indicar para o jpa que estamos trabalhando com tempo
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 
 	public long getId() {
 		return id;
@@ -61,6 +69,14 @@ public class Postagem {
 
 	public void setDate(Date date) {
 		this.date = date;
+	}
+	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
